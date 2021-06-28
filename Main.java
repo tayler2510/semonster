f7f08eaa6ad43c1ea3a3072e851d4e833724b8b4
tayler2510 <m1m21a09@st.oit.ac.jp>
2021-06-28 14:14:32 +0900

    Main.java

--- Main.java
+++ Main.java
@@ -1,7 +1,9 @@
 public class Main {
   public static void main(String[] args) {
-    System.out.println("SEMonster!");
+    System.out.println("SEMONSTER!!");
     Monster monster = new Monster();
-    System.out.println(monster.toString());
+    Player player = new Player();
+    player.drawMonsters();
+    player.showDeck();
   }
 }
